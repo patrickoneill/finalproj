ee6a8373a8cc36abb65355467cbbc97ae177c7c6
patrickoneill <patrickonei@gmail.com>
2020-06-10 22:59:36 +0000

    creating page templates for first app

--- ecomm_final/merch/views.py
+++ ecomm_final/merch/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
 
 # Create your views here.
