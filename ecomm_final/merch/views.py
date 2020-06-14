6e919ee8a608f6980d2cc050423c098262b9d1b7
patrickoneill <patrickonei@gmail.com>
2020-06-14 21:39:03 +0000

    created the files path defs to be called and the urls for the links

--- ecomm_final/merch/views.py
+++ ecomm_final/merch/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def product(request):
+    return render(request, 'product.html')
+
 def about(request):
     return render(request, 'about.html')
 
