5e1608450d4ba46589a2ef5ad1db3cb71f8ca20f
patrickoneill <patrickonei@gmail.com>
2020-06-16 20:13:56 +0000

    set up the admin models and migrations for the app

--- ecomm_final/merch/models.py
+++ ecomm_final/merch/models.py
@@ -1,25 +1,35 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=250, uniqui=True)
-    slug = models.SlugField(max_length=250, uniqui=True)
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category', blank=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str_(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=250, uniqui=True)
-    slug = models.SlugField(max_length=250, uniqui=True)
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCASE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updates = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
 
     def __str__(self):
         return self.name
