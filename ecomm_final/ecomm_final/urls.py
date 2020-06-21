4077be75081b9e8074bbbbbb3f4ed6cb0721e1ae
patrickoneill <patrickonei@gmail.com>
2020-06-21 22:48:32 +0000

    added the cart app and the url paths and added to the setting app list

--- ecomm_final/ecomm_final/urls.py
+++ ecomm_final/ecomm_final/urls.py
@@ -18,10 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('merch.urls')),
-
+    path('cart/', include('cart.urls')),
 ]
 
 if settings.DEBUG:
