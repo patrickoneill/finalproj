b3c346d181483509e8398eec8f9bec8f9b053952
patrickoneill <patrickonei@gmail.com>
2020-06-21 20:58:34 +0000

    creating the category and products in the home def to be displayed on the page

--- ecomm_final/merch/views.py
+++ ecomm_final/merch/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-def home(request):
-    return render(request, 'index.html')
+def home(request, category_slug=None):
+    category_page = None
+    products  = None
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'index.html', {'category': category_page, 'products': products})
 
-def product(request):
-    return render(request, 'product.html')
+def productPage(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'product.html', {'product': product})
 
 def about(request):
     return render(request, 'about.html')
