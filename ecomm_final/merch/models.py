e9ee6603172166089a8ec02967f12e8dd29267bc
patrickoneill <patrickonei@gmail.com>
2020-06-27 23:28:27 +0000

    created the models for orders and reviews

--- ecomm_final/merch/models.py
+++ ecomm_final/merch/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+# Model: Category
+
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -8,15 +12,18 @@ class Category(models.Model):
     image = models.ImageField(upload_to='category', blank=True)
 
     class Meta:
-        ordering = ('name', )
+        ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
     def get_url(self):
         return reverse('products_by_category', args=[self.slug])
 
-    def __str_(self):
+    def __str__(self):
         return self.name
+
+# Model: Product
+
 
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -28,22 +35,26 @@ class Product(models.Model):
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    updates = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('name', )
+        ordering = ('name',)
         verbose_name = 'product'
         verbose_name_plural = 'products'
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.name
 
+# Model: Cart
+
+
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
-    
-    
-    
+
     class Meta:
         db_table = 'Cart'
         ordering = ['date_added']
@@ -51,11 +62,13 @@ class Cart(models.Model):
     def __str__(self):
         return self.cart_id
 
+
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     active = models.BooleanField(default=True)
+
     class Meta:
         db_table = 'CartItem'
 
@@ -64,3 +77,53 @@ class CartItem(models.Model):
 
     def __str__(self):
         return self.product
+
+# Model: Order
+
+
+class Order(models.Model):
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='EURO Order Total')
+    emailAddress = models.EmailField(max_length=250, blank=True, verbose_name='Email Address')
+    created = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=250, blank=True)
+    billingCountry = models.CharField(max_length=250, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=250, blank=True)
+    shippingCountry = models.CharField(max_length=250, blank=True)
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='EURO Price')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrderItem'
+
+    def sub_total(self):
+        return self.quantity * self.price
+
+    def __str__(self):
+        return self.product
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.content
